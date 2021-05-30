1bba51008fe7c86030a511de4a538f9a0b28c812
kimkim89 <kimkim909089@gmail.com>
2021-05-31 00:46:16 +0900

    상품목록 수정 중

--- src/main/java/com/retro/adminProduct/AdminProductController.java
+++ src/main/java/com/retro/adminProduct/AdminProductController.java
@@ -78,8 +78,8 @@ public class AdminProductController {
 			
 			admProdService.adminProdInsert(adminProdVO, file1, file2, uploadPath, request);
 			
-			List<AdminProductVO> productInfoList = admProdService.adminProductInfo();
-			mav.addObject("productInfoList", productInfoList);
+			//List<AdminProductVO> productInfoList = admProdService.adminProductInfo();
+			//mav.addObject("productInfoList", productInfoList);
 			mav.setViewName("redirect:/adminProd/adminProduct");
 			return mav;
 		}
