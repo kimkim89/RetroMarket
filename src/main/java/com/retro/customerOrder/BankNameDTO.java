3486208c8afa6a43d00cbbc1277ca26f2f9cccca
kimkim89 <kimkim909089@gmail.com>
2021-10-16 23:56:06 +0900

    상품 주문 페이지 디자인 작업 완료

---
+++ src/main/java/com/retro/customerOrder/BankNameDTO.java
@@ -0,0 +1,10 @@
+package com.retro.customerOrder;
+
+import lombok.Getter;
+
+@Getter
+public class BankNameDTO {
+	private int bank_idx;
+	private int bank_code;
+	private String bank_name;	
+}
--- src/main/java/com/retro/customerOrder/CustomerOrderController.java
+++ src/main/java/com/retro/customerOrder/CustomerOrderController.java
@@ -1,5 +1,11 @@
 package com.retro.customerOrder;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,14 +14,65 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/order/*")
 public class CustomerOrderController {
 	
+	@Autowired
+	CustomerOrderService csOrderService;
+	
 	
 	//구매 페이지 이동
 		@RequestMapping(value = "orderForm")
-		public ModelAndView buyPage() {
+		public ModelAndView RegisterOrder() {
 			
 			ModelAndView mav = new ModelAndView();
 			
+			//상품 주문 페이지의 결제수단 - 은행명 출력에 사용			
+			List<BankNameDTO> bankNameList = csOrderService.selectBankName();
+				
+			System.out.println("++++++++확인 start ++++++++++++++++++++++++++++++++++++++++++");
+				System.out.println("BankNameDTO타입 배열 :: " + bankNameList);
+			System.out.println("++++++++확인 end ++++++++++++++++++++++++++++++++++++++++++");
+			
+			mav.addObject("bankNameList", bankNameList);
 			mav.setViewName("order/checkout");
 			return mav;
+			
 		}
+		
+		
+		
+		
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+			
+/*		은행 정보 입력용으로만 사용한 후 주석 처리 했음
+ * 		public void insertBankInfo() {
+//			String[] bankNameArr = {"국민은행", "신한은행", "KEB하나은행", "농협은행", "기업은행", "우리은행", "카카오뱅크", "새마을금고", "우체국", "SC제일은행", "부산은행",
+//					"경남은행", "대구은행", "광주은행", "신협", "수협은행", "산업은행", "전북은행", "제주은행", "씨티은행"};
+//			
+//			int bankCode = 1000;
+//			
+//			Map<String, Object> bankInfoMap = new HashMap<String, Object>();
+//			
+//			for(int a = 0; a<bankNameArr.length; a++) {				
+//			
+//			bankInfoMap.put("bankName", bankNameArr[a]);
+//			bankInfoMap.put("bankCode", bankCode);
+//			
+//			csOrderService.insertBankInfo(bankInfoMap); 				
+//			bankCode++;
+		}*/
+			
+
 }
